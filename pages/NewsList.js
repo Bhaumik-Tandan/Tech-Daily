f473f0397aaaa5455a227bf0dc9b46fb37e09fff
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-16 23:20:26 +0530

    [ADD] fix

--- pages/NewsList.js
+++ pages/NewsList.js
@@ -1,78 +1,70 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, FlatList, Button, ActivityIndicator, Text } from 'react-native';
 import NewsArticle from '../component/NewsArticle';
-import {API_URL} from "@env";
+import { API_URL } from "@env";
 
+const apiUrl = API_URL + "/news";
 
+const NewsList = ({ route }) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
-  const apiUrl = API_URL+"/news";
-  const NewsList = ({route}) => {
-    const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-  
-    const flatListRef = useRef();
-    const isFetching = useRef(false);
+  const flatListRef = useRef();
+  const isFetching = useRef(false);
 
-    useEffect(() => {
-      fetchNewsData();
-    }, [page]);
-  
-    const fetchNewsData = () => {
-      if (isFetching.current || page > totalPages) {
-        return;
-      }
-  
-  
-      isFetching.current = true;
-      setLoading(true);
-  
-      fetch(`${apiUrl}&page=${page}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setArticles([...articles, ...data.articles]);
-          setTotalPages(Math.ceil(data.totalResults / pageSize));
+  useEffect(() => {
+    fetchNewsData();
+  }, [page]);
+
+  const fetchNewsData = () => {
+    if (isFetching.current) {
+      return;
+    }
+
+    isFetching.current = true;
+    setLoading(true);
+
+    fetch(`${apiUrl}?page=${page}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length === 0) {
+          // No more articles to load
           setLoading(false);
           isFetching.current = false;
-        })
-        .catch((error) => {
-          console.error(error);
+        } else {
+          setArticles([...articles, ...data]);
           setLoading(false);
           isFetching.current = false;
-        });
-    };
-  
-    const loadMoreData = () => {
-      if (page < totalPages) {
-        setPage(page + 1);
-      }
-    };
-  
-    return (
-      <View>
-        <FlatList
-          ref={flatListRef}
-          data={articles}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => <NewsArticle article={item} />}
-          onEndReached={loadMoreData}
-          onEndReachedThreshold={0.5}
-          ListFooterComponent={loading ? <ActivityIndicator size="large" color="#0000ff" /> : null}
-        />
-        {page < totalPages && (
-          <Button title="Load More" onPress={loadMoreData} />
-        )}
-        {page === totalPages && (
-          <Text>No more articles to load.</Text>
-        )}
-      </View>
-    );
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+        isFetching.current = false;
+      });
   };
-  
-  export default NewsList;
-  
-  
-  
-  
-  
+
+  const loadMoreData = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <View>
+      <FlatList
+        ref={flatListRef}
+        data={articles}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => <NewsArticle article={item} />}
+        onEndReached={loadMoreData}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={loading ? <ActivityIndicator size="large" color="#0000ff" /> : null}
+      />
+      {loading && (
+        <ActivityIndicator size="large" color="#0000ff" />
+      )}
+    </View>
+  );
+};
+
+export default NewsList;
