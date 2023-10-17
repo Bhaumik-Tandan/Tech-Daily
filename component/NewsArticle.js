5480e5314709ac0ebda2fdadd5e23d5b4dbf52dc
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-17 20:19:15 +0530

    [ADD] share box

--- component/NewsArticle.js
+++ component/NewsArticle.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { TouchableOpacity, Text, Image, Dimensions, StyleSheet, View } from 'react-native';
+import { TouchableOpacity, Text, Image, Dimensions, StyleSheet, View,Share } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import PAGES from '../utils/constants/pages';
+import { AntDesign } from '@expo/vector-icons';
+
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -10,46 +12,57 @@ const NewsArticle = ({ article,index }) => {
     title,
     summary,
     sourceURL,
-    image
+    image,
+    _id
   } = article;
   const navigation = useNavigation()
   const isEvenIndex = index % 2 === 0;
+  const ImageContainer=()=>(image ? (
+    <Image
+      source={{ uri: image }}
+      style={styles.image}
+      resizeMode="cover"
+    /> 
+  ):
+  <Image
+  source={{uri:"https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg"}}
+  style={styles.image}
+  resizeMode='cover'
+  />);
   return (
+    <View
+    style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      position:'relative',
+      borderColor: '#ddd', // Add a border for separation
+      borderBottomWidth: 1, // Border width
+      padding: 10, // Add padding for spacing
+    }}
+    >
     <TouchableOpacity
       onPress={() => navigation.navigate(PAGES.SINGLE_NEWS_PAGE, { url: sourceURL })}
       style={styles.container}
+
     >
-      {isEvenIndex && (image ? (
-        <Image
-          source={{ uri: image }}
-          style={styles.image}
-          resizeMode="cover"
-        /> 
-      ):
-      <Image
-      source={{uri:"https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg"}}
-      style={styles.image}
-      resizeMode='cover'
-      />
-      )}
+      {isEvenIndex && <ImageContainer/>}
       <View style={styles.textContainer}>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.description}>{summary}</Text>
       </View>
-      {!isEvenIndex && (image ? (
-        <Image
-          source={{ uri: image }}
-          style={styles.image}
-          resizeMode="cover"
-        /> 
-      ):
-      <Image
-      source={{uri:"https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg"}}
-      style={styles.image}
-      resizeMode='cover'
-      />
-      )}
+      {!isEvenIndex && <ImageContainer/>}
     </TouchableOpacity>
+    <TouchableOpacity onPress={async () => {
+      await Share.share({
+        message:
+          `Check out this news: https://api-tech-daily.cyclic.app/link?redirect=news?id=${_id}`,
+      })
+}}>
+  <AntDesign name="sharealt" size={30} color="rgba(0, 0, 0, 0.5)" />
+
+    </TouchableOpacity>
+    </View>
   );
 };
 
@@ -58,9 +71,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    borderColor: '#ddd', // Add a border for separation
-    borderBottomWidth: 1, // Border width
-    padding: 10, // Add padding for spacing
   },
   image: {
     width: windowWidth / 3, // Adjust the width to your preference
@@ -79,7 +89,7 @@ const styles = StyleSheet.create({
     fontSize: 14, // Adjust the font size to your preference
     color: '#666', // Adjust the color to your preference
     marginTop: 5, // Add margin for spacing
-  },
+  }
 });
 
 export default NewsArticle;
