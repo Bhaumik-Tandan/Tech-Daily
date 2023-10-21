4ee8cadc372b7f13ea77afde228cbb144dd56be2
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-21 23:39:33 +0530

    [FIX] infite scroll flatlist

--- pages/NewsList.js
+++ pages/NewsList.js
@@ -1,22 +1,22 @@
+import { View, Text, Image, FlatList, StyleSheet, Dimensions } from 'react-native';
 import React, { useState, useEffect, useRef } from 'react';
-import { View, FlatList, ActivityIndicator } from 'react-native';
-import NewsArticle from '../component/NewsArticle';
 import { API_URL } from "@env";
 import Loader from '../component/Loader';
+import Constants from 'expo-constants';
 
 const apiUrl = API_URL + "/news";
+const window = Dimensions.get('window');
 
-const NewsList = ({ route }) => {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
+
+export default function App() {
+    const [articles, setArticles] = useState([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
   const [isLoadingMore, setIsLoadingMore] = useState(false); // New loading flag
 
-  const flatListRef = useRef();
 
   const fetchNewsData = async () => {
-    if (!hasMore || loading || isLoadingMore) {
+    if (!hasMore  || isLoadingMore) {
       return;
     }
 
@@ -46,29 +46,52 @@ const NewsList = ({ route }) => {
     fetchNewsData();
   }, []);
 
-  const loadMoreData = () => {
-    if (!loading) {
-      fetchNewsData(); // Load more data when reaching the end
-    }
-  };
-
   return (
-    <View>
-      <FlatList
-        ref={flatListRef}
-        data={articles}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item,index }) => <NewsArticle article={item} index={index} />}
-        onEndReached={loadMoreData}
-        onEndReachedThreshold={0.5}
-        ListFooterComponent={isLoadingMore ? <Loader /> : null}
-        pagingEnabled={true}
-      />
-      {loading && !hasMore && (
-        <Loader />
+    <FlatList
+      data={articles}
+      renderItem={({ item }) => (
+        <View style={styles.newsCard}>
+          <Image source={{ uri: item.image }} style={styles.newsImage} />
+          <Text style={styles.newsTitle}>{item.title}</Text>
+          <Text style={styles.newsSummary}>{item.summary}</Text>
+        </View>
       )}
-    </View>
+      keyExtractor={(item) => item._id}
+      pagingEnabled
+      horizontal={false} // Set to true for vertical scrolling
+      showsVerticalScrollIndicator={false} // Hide vertical scroll indicator
+      style={styles.container}
+      onEndReached={fetchNewsData}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={isLoadingMore ? <Loader /> : null}
+    />
   );
-};
+}
 
-export default NewsList;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
+  newsCard: {
+    height: window.height-Constants.statusBarHeight, // Occupies the entire screen's height
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    borderRadius: 5,
+  },
+  newsImage: {
+    width: '100%',
+    height: 200, // Adjust the height as needed
+  },
+  newsTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  newsSummary: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+});
